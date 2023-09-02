abed98f8f2891d854c2d6647b2bc9b6fa4238982
Darley Peralta <ordeal00s@gmail.com>
2023-09-02 18:53:32 -0400

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,34 @@
 // STRETCH
+ const cars = [
+    {
+        vin: '1111181111111',
+        make: "honda",
+        model: 'civic',
+        mileage: 12765,
+        transmission: 'auto'
+
+    },
+    {
+        vin: '1111191111111',
+        make: "toyota",
+        model: 'avalon',
+        mileage: 12987,
+        
+    },
+    {
+        vin: '1111111911111',
+        make: "Honda",
+        model: 'accord',
+        mileage: 122000,
+        
+    }
+ ]
+
+
+
+ exports.seed =  async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+
+   
+ }
